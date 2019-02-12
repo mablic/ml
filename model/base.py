d84d543730860ea314d797d6518c6c9db90b0075
mablic <maihe88@gmail.com>
2019-02-11 22:07:03 -0600

    change the regression to base class, add regression and logistic model

---
+++ model/base.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+PATH = 'D:\\python\\ML\\model\\data\\data1.txt'
+
+
+class Data:
+
+    def __init__(self):
+        self.theta = 0
+        self.ans = []
+        self.df_x = pd.DataFrame()
+        self.df_y = pd.Series()
+
+    def readData(self, *args, path=PATH):
+        df = pd.read_csv(path, header=None)
+        columns = []
+        for i in args:
+            columns.append(i)
+        df.columns = columns
+        return df
+
+    def setY(self, y_column):
+
+        yList = []
+        try:
+            yList = self.df[y_column].tolist()
+        except ValueError:
+            print('%s is not valid.' % y_column)
+        return pd.Series(yList)
+        # self.__yColName = y_column
+
+    def setX(self, *args, valueParam=True):
+        # valueParam = True means first column of the data is X0 data
+        # it contains constant value
+        # this feature is apply for linear regression
+
+        columns = self.df.columns.tolist()
+        xDict = {}
+        nameSet = set()
+        for i in range(len(args)):
+            if args[i] not in nameSet:
+                nameSet.add(args[i])
+
+        for i in range(len(columns)):
+            if columns[i] in nameSet and columns[i] not in xDict.keys():
+                xDict[columns[i]] = self.df[columns[i]]
+        ttl = len(self.df)
+        xData = pd.DataFrame(xDict)
+        if valueParam:
+            tmpData = pd.DataFrame([1 for _ in range(ttl)], columns=['fst'])
+            return tmpData.join(xData)
+        else:
+            return xData
+
+
+class Graph:
+    def __init__(self):
+        pass
+
+    def graph(self, theta, df, df_x, df_y):
+        fig, ax = plt.subplots(figsize=(6, 4))
+        tmp = theta * df_x
+        df['predict'] = tmp.sum(axis=1)
+        # plt.scatter(self.df.values)
+        ax.plot(df['predict'], 'o', label='Prediction', color='g')
+        ax.plot(df_y, '^', label='Ground Truth', color='r')
+        plt.xlabel('x-axis')
+        plt.ylabel('y-axis')
+        plt.title('Prediction vs Actual')
+        plt.legend(loc='upper right')
+        plt.show()
+
+
+if __name__ == '__main__':
+
+    TEST = Data()
+    TEST.readData('x', 'y')
+    TEST.setX('x')
+    TEST.setY('y')
+
+    # df = read_data('x', 'y')
+    # theta = [0, 0]
+    # print(computeCost(df, theta))
+    # predict = gradient_descent(df, theta, 0.01, 500)
+    # print(predict)
+    # graph_predict(df, theta)
+    # theta = np.array([0, 0])
+    # test = GradientDescent(df)
+    # test.setX('x')
+    # test.setY('y')
+    # ret = test.computeCost(theta)
+    # ret, ans = test.computeGradientDescent(theta, 0.01, 500)
+    # test.graph(ret)
+    # print(ret)
---
+++ model/regression.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import math
+import model.base as base
+
+PATH1 = 'D:\\python\\ML\\model\\data\\data1.txt'
+PATH2 = 'D:\\python\\ML\\model\\data\\log-data1.txt'
+
+
+# this module contains regression class
+# gradientdescent, logistic class
+# this is the gradientdescent for linear regression
+class GradientDescent(base.Data, base.Graph):
+
+    def __init__(self, *args, path):
+        self.theta = 0
+        self.ans = []
+        self.df_x = pd.DataFrame()
+        self.df_y = pd.Series()
+        self.df = super().readData(*args, path=path)
+
+    def setX(self, *args):
+        self.df_x = super().setX(*args, valueParam=True)
+
+    def setY(self, y_column):
+        self.df_y = super().setY(y_column)
+
+    def computeCost(self, theta):
+
+        if len(self.df_y) == 0 or len(self.df_x) == 0:
+            return 0
+
+        ttl = len(self.df)
+
+        # return xData
+        cost = 1 / (2 * ttl) * sum((np.array(self.df_x).dot(theta) - self.df_y) ** 2)
+        return cost
+
+    def computeGradientDescent(self, theta, alpha, nums):
+        if len(self.df_y) == 0 or len(self.df_x) == 0:
+            return 0
+        ttl = len(self.df)
+        ans = []
+
+        for i in range(nums):
+            theta = theta - alpha * (1/ttl) * \
+                    (np.transpose(np.array(self.df_x).dot(theta) - self.df_y).dot(np.array(self.df_x)))
+            self.ans.append(self.computeCost(theta))
+        self.theta = theta
+
+    def graph(self):
+        super().graph(self.theta, self.df, self.df_x, self.df_y)
+
+
+class LogisticRegression(base.Data, base.Graph):
+
+    def __init__(self, *args, path):
+        self.theta = []
+        self.ans = []
+        self.df_x = pd.DataFrame()
+        self.df_y = pd.Series()
+        self.df = super().readData(*args, path=path)
+
+    def setX(self, *args):
+        self.df_x = super().setX(*args, valueParam=True)
+
+    def setY(self, y_column):
+        self.df_y = super().setY(y_column)
+
+    def sigmoidFunction(self, z):
+        return 1 / (1 + math.exp(-z))
+
+    def computeCost(self, theta):
+        # this is for the cost computation for logistic regression
+        # it apply different cost function compare to linear regression
+        if len(self.df_y) == 0 or len(self.df_x) == 0:
+            return 0
+
+        ttl = len(self.df)
+        z = self.df_x.dot(np.array(theta))
+        sigmoid = z.apply(self.sigmoidFunction)
+        # compute Cost
+        cost = (1 / ttl)*sum(-self.df_y * np.log(sigmoid)-(1-self.df_y)* np.log(1 - sigmoid))
+        return cost
+
+    def computeGradientDescent(self, theta, alpha, nums):
+        if len(self.df_y) == 0 or len(self.df_x) == 0:
+            return 0
+        ttl = len(self.df)
+        # ans = []
+        theta = np.array(theta)
+        for i in range(nums):
+            z = self.df_x.dot(theta)
+            sigmoid = z.apply(self.sigmoidFunction)
+            theta = theta - alpha * (1/ttl) * (np.transpose(sigmoid - self.df_y)).dot(np.array(self.df_x))
+            self.ans.append(self.computeCost(theta))
+        self.theta = theta
+
+
+if __name__ == '__main__':
+    # testObj = GradientDescent('x', 'y', path=PATH1)
+    # testObj.setX('x')
+    # testObj.setY('y')
+    # theta = [0, 0]
+    # testObj.computeGradientDescent(theta, 0.01, 500)
+    # testObj.graph()
+    testObj = LogisticRegression('x1', 'x2', 'y', path=PATH2)
+    testObj.setX('x1', 'x2')
+    testObj.setY('y')
+    theta = [0, 0, 0]
+    testObj.computeGradientDescent(theta, 0.005, 100000)
+
+    print(testObj.theta)
+    # print(testObj.ans[-1])
+    # print(testObj.ans)
+    # print(testObj.computeCost(theta))
+
+
